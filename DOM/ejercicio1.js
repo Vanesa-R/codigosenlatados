503ce276175dbd7d69d0150761a8b0e3ebd817a6
Vanesa <uve.rdguez@gmail.com>
2020-06-02 13:03:45 +0200

    Actualización de documentación
    Estructuras de control

---
+++ DOM/ejercicio1.js
@@ -0,0 +1,7 @@
+// Ejercicio 1: CREANDO NODOS
+
+let elemento = document.createElement("p"), //Crear elemento
+    contenido = document.createTextNode("Proin congue nibh libero mauris quam ridiculus, facilisis ultricies eu habitant et ligula semper, nisl justo montes inceptos orci. Scelerisque duis felis quam vel eget congue nostra litora cubilia, eros facilisi ultrices vulputate hendrerit arcu vehicula natoque nulla, rhoncus neque ultricies mauris viverra ante gravida potenti."); // Crear su contenido
+
+elemento.appendChild(contenido); // Agregar el contenido al elemento
+document.getElementById("parrafo").appendChild(elemento); // Agregamos el nuevo elemento
