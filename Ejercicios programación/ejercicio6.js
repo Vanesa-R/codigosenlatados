0522aab7a34ccfcf6b86a24c0d86fd6a9031e1c7
Vanesa <uve.rdguez@gmail.com>
2020-07-16 16:30:00 +0200

    Ejercicios de programación

--- Ejercicios programación/ejercicio6.js
+++ Ejercicios programación/ejercicio6.js
@@ -17,10 +17,10 @@ if (num1 <= num2 && num1 <= num3){
     } else {
         console.log(`El orden de los números, de menor a mayor es: ${num2}, ${num3} y ${num1}`);
     }
-} else (num3 <= num1 && num3 <= num2){
+} else if (num3 <= num1 && num3 <= num2){
     if (num1 <= num2){
         console.log(`El orden de los números, de menor a mayor es: ${num3}, ${num1} y ${num2}`);
-    } else if {
+    } else {
         console.log(`El orden de los números, de menor a mayor es: ${num3}, ${num2} y ${num1}`);
     }
 }
