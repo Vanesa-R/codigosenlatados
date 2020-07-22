9de3afdeb09d18c62c56d5de1b497e2a532accee
Vanesa <uve.rdguez@gmail.com>
2020-07-22 10:42:09 +0200

    Objetos en JavaScript

--- Objetos/Objetos nativos/array.js
+++ Objetos/Objetos nativos/array.js
@@ -2,7 +2,7 @@
 
     let month = new Array (); // No se recomienda su uso
 
-    const month = ["enero", "febrero", "marzo", "abril"] // Podemos almacenar números, booleanos… es suficiente con crear un array declarando una variable y almacenar los valores entre corchetes.
+    let month = ["enero", "febrero", "marzo", "abril"] // Podemos almacenar números, booleanos… es suficiente con crear un array declarando una variable y almacenar los valores entre corchetes.
 
 
 /* Propiedades y métodos útiles: */
@@ -17,43 +17,64 @@
 
     // shift: Eliminar el primer elemento del array
     // pop: Eliminar el último elemento del array
-    colors.pop(); // Devuelve y elimina del array «amarillo»
+    console.log(colors.pop()); // Devuelve y elimina del array «amarillo»
 
     // unshift: Añade un elemento al principio del array
     // push: Añade un elemento al final del array
-    colors.push("rojo"); // Añade «rojo» al final del array «colores»
+    console.log(colors.push("rojo")); // Añade «rojo» al final del array «colores»
 
     // indexOf: Primer índice del elemento que coincida con el valor indicado
     // lastIndexOf, por el contrario, devuelve el último
-    colors.indexOf("verde"); // Devuelve «1»
+    console.log(colors.indexOf("verde")); // Devuelve «1»
 
     // reverse: Invierte el orden de los elementos
-    colors.reverse(); // Devuelve «(4) ["amarillo", "violeta", "verde", "naranja"]»
+    console.log(colors.reverse()); // Devuelve «(4) ["amarillo", "violeta", "verde", "naranja"]»
 
     // join: Separador, devuelve el string con el separador que indiquemos (por defecto: comas sin espacio)
-    colors.join(); // Devuelve «naranja,verde,violeta,amarillo»
-    colors.join(", "); // Devuelve «naranja, verde, violeta, amarillo»
+    console.log(colors.join()); // Devuelve «naranja,verde,violeta,amarillo»
+    console.log(colors.join(", ")); // Devuelve «naranja, verde, violeta, amarillo»
 
     // splice: Cambia contenido del array. Elimina el contenido que indicamos y añade nuevo contenido
-    colors.splice(0, 2, "rojo", "azul"); // Devuelve  «["rojo", "azul", "violeta", "amarillo"]» Se posiciona en el indicador a (0), y modifica hasta el indicador b (2), añadiendo el nuevo contenido.
-    colors.splice(2, 0, "naranja"); // Devuelve «["rojo", "azul", "naranja", "violeta", "amarillo"]» pues si el indicador b es cero no elimina nada
+    console.log(colors.splice(0, 2, "rojo", "azul")); // Devuelve  «["rojo", "azul", "violeta", "amarillo"]» Se posiciona en el indicador a (0), y modifica hasta el indicador b (2), añadiendo el nuevo contenido.
+    console.log(colors.splice(2, 0, "naranja")); // Devuelve «["rojo", "azul", "naranja", "violeta", "amarillo"]» pues si el indicador b es cero no elimina nada
 
     // slice: Extrae elementos de un array desde el indicador a al b.
     colors = ["rojo", "azul", "naranja", "violeta", "amarillo"];
-    colors.slice(2,3); // Devuelve «naranja»
+    console.log(colors.slice(2,3)); // Devuelve «naranja»
+
+    // sort: Ordena los elementos de un array alfabéticamente. Puede ordenarlos según el algoritmo que le pasemos en el callback.
+    colors = ["rojo", "azul", "naranja", "violeta", "amarillo"];
+    console.log(colors.sort()); // Devuelve «["amarillo", "azul", "naranja", "rojo", "violeta"]»
 
     // forEach: Recorrer array
-    colors.forEach(function(e){
-        console.log(e);
-    });
+    colors.forEach(e => console.log(e));
+    /* El método forEach recibe una función de tipo flecha. Para ver claramente el ejemplo sin nociones 
+    de este tipo de funciones:
 
-    //Find
-    console.log(colors.find (color => color.length > 7)); //Devuelve «amarillo»
-    
-    /* El método find recibe una función de tipo flecha, que ha sido optimizada para mejorar su legibilidad. 
-    Para ver más claramente el ejemplo sin nociones de este tipo de funciones:
+        colors.forEach(function(e){
+            console.log(e);
+        }); */
+   
+    // find
+    console.log(colors.find (color => color.length > 4)); //Devuelve naranja
+    /* console.log(colors.find(function(color){
+        return color.length > 7;
+    })); */
 
-        console.log(colores.find((color) => { 
-            return color.length > 7; 
-        }));
-    */
+    // some: Comprueba si al menos un elemento del array cumple la condición.
+    console.log(colors.some(color => color.length > 6 )); // Devuelve «true» pues el elemento «naranja» cumple la condición
+    /* console.log(colors.some(function(color){
+        return color.length > 6;
+    })); */
+
+    // every: Comprueba si todos los elementos del array cumplen la condición.
+    console.log(colors.every(color => color.length > 6)); // Devuelve «false» pues solo un elemento cumple la condición
+    /* console.log(colors.every(function(color){
+        return color.length > 6;
+    })); */
+
+    // filter: Filtra los elementos del array que cumplan la condición y devuelve otro array
+    console.log(colors.filter(color => color.length > 4)); // Devuelve «"Naranja", "violeta", "amerillo», pues "rojo y "azul" quedan fuera del filtrado
+    /* console.log(colors.filter(function(color){
+        return color.length > 4;
+    })); */
