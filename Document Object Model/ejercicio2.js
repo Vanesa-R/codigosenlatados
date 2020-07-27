f7f5048fedfc35cdd8c0e53f357c8d7fceaabd0b
Vanesa <uve.rdguez@gmail.com>
2020-07-27 11:49:54 +0200

    Document Object Model

--- Document Object Model/ejercicio2.js
+++ Document Object Model/ejercicio2.js
@@ -1,7 +1,8 @@
-// Ejercicio 1: CREANDO NODOS
+// Ejercicio 2: CREANDO NODOS
+
 
 let elemento = document.createElement("p"), //Crear elemento
     contenido = document.createTextNode("Proin congue nibh libero mauris quam ridiculus, facilisis ultricies eu habitant et ligula semper, nisl justo montes inceptos orci. Scelerisque duis felis quam vel eget congue nostra litora cubilia, eros facilisi ultrices vulputate hendrerit arcu vehicula natoque nulla, rhoncus neque ultricies mauris viverra ante gravida potenti."); // Crear su contenido
 
 elemento.appendChild(contenido); // Agregar el contenido al elemento
-document.getElementById("parrafo").appendChild(elemento); // Agregamos el nuevo elemento
+document.getElementById("paragraph").app
---
+++ Document Object Model/eventos.js
@@ -0,0 +1,4 @@
+/* PARTE 3: EVENTOS */
+
+const btn = document.getElementById("btn-saludo");
+btn.addEventListener("click", () => alert(`¡Hola!`));
