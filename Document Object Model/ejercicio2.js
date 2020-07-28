68bfe1ae754ace41c0e0c6a611ad59b40905259d
Vanesa <uve.rdguez@gmail.com>
2020-07-28 09:44:40 +0200

    Document Object Model

--- Document Object Model/ejercicio2.js
+++ Document Object Model/ejercicio2.js
@@ -5,4 +5,4 @@ let elemento = document.createElement("p"), //Crear elemento
     contenido = document.createTextNode("Proin congue nibh libero mauris quam ridiculus, facilisis ultricies eu habitant et ligula semper, nisl justo montes inceptos orci. Scelerisque duis felis quam vel eget congue nostra litora cubilia, eros facilisi ultrices vulputate hendrerit arcu vehicula natoque nulla, rhoncus neque ultricies mauris viverra ante gravida potenti."); // Crear su contenido
 
 elemento.appendChild(contenido); // Agregar el contenido al elemento
-document.getElementById("paragraph").app
+document.getElementById("paragraph").appendChild(elemento);
