66c44e9a77aa77fd27b60377340c592eefee7ac6
Vanesa <uve.rdguez@gmail.com>
2020-07-28 15:54:29 +0200

    Document Object Model

--- Document Object Model/ejercicio2.js
+++
@@ -1,8 +0,0 @@
-// Ejercicio 2: CREANDO NODOS
-
-
-let elemento = document.createElement("p"), //Crear elemento
-    contenido = document.createTextNode("Proin congue nibh libero mauris quam ridiculus, facilisis ultricies eu habitant et ligula semper, nisl justo montes inceptos orci. Scelerisque duis felis quam vel eget congue nostra litora cubilia, eros facilisi ultrices vulputate hendrerit arcu vehicula natoque nulla, rhoncus neque ultricies mauris viverra ante gravida potenti."); // Crear su contenido
-
-elemento.appendChild(contenido); // Agregar el contenido al elemento
-document.getElementById("paragraph").appendChild(elemento);
