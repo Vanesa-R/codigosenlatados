330c70379894332ff0fafbddc9e1851240d30041
Vanesa <uve.rdguez@gmail.com>
2020-07-31 11:33:41 +0200

    Objetos en JavaScript

--- Objetos/Objetos de alto nivel/navigator.js
+++ Objetos/Objetos de alto nivel/navigator.js
@@ -2,8 +2,8 @@
 
 // Propiedades
 navigator.appCodeName;
-appName; // Devuelve el nombre del navegador
-appVersion; // Devuelve la versión del navegador
-language; // Devuelve el lenguaje del navegador
-cookieEnabled // Indica sin las cookies están habilitadas en el navegador
-geolocation // Devuelve un objeto que puede utilizarse para localizar la posición del usuario
+navigator.appName; // Devuelve el nombre del navegador
+navigator.appVersion; // Devuelve la versión del navegador
+navigator.language; // Devuelve el lenguaje del navegador
+navigator.cookieEnabled // Indica sin las cookies están habilitadas en el navegador
+navigator.geolocation // Devuelve un objeto que puede utilizarse para localizar la posición del usuario
--- Objetos/README.md
+++ Objetos/README.md
@@ -4,8 +4,18 @@
 [Leer introducción a los objetos](https://github.com/Vanesa-R/javascript/blob/master/Objetos%20nativos/objetos.js)
 
 ## Tipos de objetos
+
+### Objetos nativos
  - String
  - Number
  - Boolean
  - Array
  - Math
+ - Date
+
+### Objetos de alto nivel
+ - Window
+ - Screen
+ - Navigator
+ - Location
+ - History
